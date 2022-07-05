e5af4ed1fc1fd6d3fceb21a20591bb2e9baa7181
Nikolay Ivanov <eoneof@yandex.ru>
2022-07-06 01:24:45 +0300

    refactor: pass children nodes to popup component
    wip

--- src/components/App.js
+++ src/components/App.js
@@ -9,54 +9,151 @@ import PopupWithImage from './PopupWithImage.js';
 import Card from './Card.js';
 
 function App() {
-  const [isPopupOpen, setIsPopoupOpen] = React.useState(false);
+  const [isAddPopupOpen, setIsAddPopoupOpen] = React.useState(false);
+  const [isEditPopupOpen, setIsEditPopupOpen] = React.useState(false);
+  const [isUpdatePopupOpen, setIsUpdatePopupOpen] = React.useState(false);
+  const [isPreviewPopupOpen, setIsPreviewPopupOpen] = React.useState(false);
 
   function handleCloseButton() {
-    setIsPopoupOpen(false);
+    setIsUpdatePopupOpen(false);
+    setIsEditPopupOpen(false);
+    setIsAddPopoupOpen(false);
+    setIsPreviewPopupOpen(false);
   }
 
   function handleUpdateAvatar() {
-    setIsPopoupOpen(true);
+    setIsUpdatePopupOpen(true);
   }
 
   function handleEditProfile() {
-    setIsPopoupOpen(true);
+    setIsEditPopupOpen(true);
   }
+
   function handleAddCArd() {
-    setIsPopoupOpen(true);
+    setIsAddPopoupOpen(true);
   }
+
   function handleCardPreview() {
-    setIsPopoupOpen(true);
+    setIsPreviewPopupOpen(true);
   }
+
+  // FIXME put it somewhere
+  // function handleEscClose(evt) {
+  //   if (evt.key === 'Escape') {
+  //     setIsPopoupOpen(false);
+  //   }
+  // }
 
   // function handleAddPlaceClick() {
   //   setIsPopoupOpen(true);
   // }
+
   return (
     <div className='page'>
       <Header />
       <Preloader />
+
       <Main
         onUpdateAvatar={handleUpdateAvatar}
         onEditProfile={handleEditProfile}
         onAddCard={handleAddCArd}
         onCardClick={handleCardPreview}
       />
+
       <Footer />
+      {/* FIXME input containers shift backdrops as they are rendered the wrong node (where?) */}
       <PopupWithForm
-        formTitle='<TITLE>' /* TODO get from external */
-        formName='<NAME>' /* TODO get from external */
-        isOpen={isPopupOpen}
+        formTitle='Редактировать профиль' /* TODO get from external */
+        formName='form-edit' /* TODO get from external */
+        isOpen={isEditPopupOpen}
         buttonText='Сохранить'
         onPopupClose={handleCloseButton}
-      />
+        /* onEscape={handleEscClose} */
+      >
+        <>
+          <div className='form__input-container'>
+            <input
+              className='form__input'
+              id='photoNameInput'
+              name='name'
+              type='text'
+              minLength='2'
+              maxLength='30'
+              placeholder='Название'
+              required
+            />
+            <span className='form__input-error-hint name-input-error'></span>
+          </div>
+          <div className='form__input-container'>
+            <input
+              className='form__input'
+              id='photoLinkInput'
+              name='link'
+              type='url'
+              placeholder='Ссылка на картинку'
+              required
+            />
+            <span className='form__input-error-hint link-input-error'></span>
+          </div>
+        </>
+      </PopupWithForm>
+
       <PopupWithForm
-        formTitle='<TITLE>' /* TODO get from external */
-        formName='<NAME>' /* TODO get from external */
-        isOpen={isPopupOpen}
+        formTitle='Новое место'
+        formName='form-add'
         buttonText='Сохранить'
-        onPopupClose={handleCloseButton}
-      />
+        isOpen={isAddPopupOpen}
+        onPopupClose={handleCloseButton}>
+        <>
+          <div className='form__input-container'>
+            <input
+              className='form__input'
+              id='nameInput'
+              name='name'
+              type='text'
+              minLength='2'
+              maxLength='40'
+              placeholder='Как вас зовут?'
+              required
+            />
+            <span className='form__input-error-hint name-input-error'></span>
+          </div>
+          <div className='form__input-container'>
+            <input
+              className='form__input'
+              id='aboutInput'
+              name='about'
+              type='text'
+              minLength='2'
+              maxLength='200'
+              placeholder='Напишите что-нибудь о себе'
+              required
+            />
+            <span className='form__input-error-hint about-input-error'></span>
+          </div>
+        </>
+      </PopupWithForm>
+
+      <PopupWithForm
+        formTitle='Обновить аватар' /* TODO get from external */
+        formName='form-update' /* TODO get from external */
+        buttonText='Сохранить'
+        isOpen={isUpdatePopupOpen}
+        onPopupClose={handleCloseButton}>
+        <>
+          <div className='form__input-container'>
+            <input
+              className='form__input'
+              id='updateInput'
+              name='avatar'
+              type='url'
+              placeholder='Ссылка на картинку'
+              required
+            />
+            <span className='form__input-error-hint avatar-input-error'></span>
+          </div>
+        </>
+      </PopupWithForm>
       <PopupWithImage />
       <Card />
     </div>
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -23,39 +23,12 @@ function PopupWithForm({
             onClick={onPopupClose}>
             {buttonText}
           </button>
-          <form noValidate id='form-edit' name='form-edit' className='form'>
+          <form noValidate id={formName} name={formName} className='form'>
             <h2 className='form__header'>{formTitle}</h2>
-            <fieldset className='form__fieldset'>
-              <div className='form__input-container'>
-                <input
-                  className='form__input'
-                  id='nameInput'
-                  name='name'
-                  type='text'
-                  minLength='2'
-                  maxLength='40'
-                  placeholder='Как вас зовут?'
-                  required
-                />
-                <span className='form__input-error-hint name-input-error'></span>
-              </div>
-              <div className='form__input-container'>
-                <input
-                  className='form__input'
-                  id='aboutInput'
-                  name='about'
-                  type='text'
-                  minLength='2'
-                  maxLength='200'
-                  placeholder='Напишите что-нибудь о себе'
-                  required
-                />
-                <span className='form__input-error-hint about-input-error'></span>
-              </div>
-            </fieldset>
+            <fieldset className='form__fieldset'>{children}</fieldset>
             <button
               className='button form__submit-button'
-              form='form-edit'
+              form={formName}
               type='submit'
               name='submit-button'>
               Сохранить
@@ -70,57 +43,6 @@ function PopupWithForm({
 
 export default PopupWithForm;
 
-// <section className='popup popup_type_edit'>
-//       <div className='popup__container'>
-//         <button
-//           className='button popup__close-button'
-//           type='button'
-//           name='close-button'
-//           title='Закрыть'>
-//           Закрыть
-//         </button>
-//         <form noValidate id='form-edit' name='form-edit' className='form'>
-//           <h2 className='form__header'>Редактировать профиль</h2>
-//           <fieldset className='form__fieldset'>
-//             <div className='form__input-container'>
-//               <input
-//                 className='form__input'
-//                 id='nameInput'
-//                 name='name'
-//                 type='text'
-//                 minLength='2'
-//                 maxLength='40'
-//                 placeholder='Как вас зовут?'
-//                 required
-//               />
-//               <span className='form__input-error-hint name-input-error'></span>
-//             </div>
-//             <div className='form__input-container'>
-//               <input
-//                 className='form__input'
-//                 id='aboutInput'
-//                 name='about'
-//                 type='text'
-//                 minLength='2'
-//                 maxLength='200'
-//                 placeholder='Напишите что-нибудь о себе'
-//                 required
-//               />
-//               <span className='form__input-error-hint about-input-error'></span>
-//             </div>
-//           </fieldset>
-//           <button
-//             className='button form__submit-button'
-//             form='form-edit'
-//             type='submit'
-//             name='submit-button'>
-//             Сохранить
-//           </button>
-//         </form>
-//       </div>
-//       <div className='popup__backdrop'></div>
-//     </section>
-
 //     {/* <!-- UPDATE PROFILE PHOTO --> */}
 //     <section className='popup popup_type_update'>
 //       <div className='popup__container'>
@@ -134,17 +56,6 @@ export default PopupWithForm;
 //         <form noValidate id='form-update' name='form-update' className='form'>
 //           <h2 className='form__header'>Обновить аватар</h2>
 //           <fieldset className='form__fieldset'>
-//             <div className='form__input-container'>
-//               <input
-//                 className='form__input'
-//                 id='updateInput'
-//                 name='avatar'
-//                 type='url'
-//                 placeholder='Ссылка на картинку'
-//                 required
-//               />
-//               <span className='form__input-error-hint avatar-input-error'></span>
-//             </div>
 //           </fieldset>
 //           <button
 //             className='button form__submit-button'
@@ -152,56 +63,6 @@ export default PopupWithForm;
 //             type='submit'
 //             name='submit-button'>
 //             Сохранить
-//           </button>
-//         </form>
-//       </div>
-//       <div className='popup__backdrop'></div>
-//     </section>
-
-//     {/* <!-- ADD CARD --> */}
-//     <section className='popup'>
-//       <div className='popup__container'>
-//         <button
-//           className='button popup__close-button'
-//           type='button'
-//           name='close-button'
-//           title='Закрыть'>
-//           Закрыть
-//         </button>
-//         <form noValidate id='form-add' name='form-add' className='form'>
-//           <h2 className='form__header'>Новое место</h2>
-//           <fieldset className='form__fieldset'>
-//             <div className='form__input-container'>
-//               <input
-//                 className='form__input'
-//                 id='photoNameInput'
-//                 name='name'
-//                 type='text'
-//                 minLength='2'
-//                 maxLength='30'
-//                 placeholder='Название'
-//                 required
-//               />
-//               <span className='form__input-error-hint name-input-error'></span>
-//             </div>
-//             <div className='form__input-container'>
-//               <input
-//                 className='form__input'
-//                 id='photoLinkInput'
-//                 name='link'
-//                 type='url'
-//                 placeholder='Ссылка на картинку'
-//                 required
-//               />
-//               <span className='form__input-error-hint link-input-error'></span>
-//             </div>
-//           </fieldset>
-//           <button
-//             className='button form__submit-button'
-//             form='form-add'
-//             type='submit'
-//             name='submit-button'>
-//             Создать
 //           </button>
 //         </form>
 //       </div>
