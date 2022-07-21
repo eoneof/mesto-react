180552222123c1e0718af9100b5af9c3894e31fc
Nikolay Ivanov <eoneof@yandex.ru>
2022-07-21 17:22:02 +0300

    refactor: use ref for input

--- src/components/popups/EditAvatarPopup.js
+++ src/components/popups/EditAvatarPopup.js
@@ -1,34 +1,20 @@
 import React from 'react';
-import { CurrentUserContext } from '../contexts/CurrentUserContext.js';
+import { useRef } from 'react';
 import PopupWithForm from './PopupWithForm.js';
 
 export default function EditAvatarPopup(props) {
-  const currentUser = React.useContext(CurrentUserContext);
-  const [avatar, setAvatar] = React.useState({ avatar: '' });
-
-  function updateAvatar() {
-    setAvatar({ avatar: currentUser.avatar });
-  }
-
-  function handleChange(evt) {
-    setAvatar({ avatar: evt.target.value });
-  }
+  const avatarRef = useRef();
 
   function handleSubmit(evt) {
     evt.preventDefault();
-    props.onSubmitAvatar(avatar);
+    props.onSubmitAvatar({ avatar: avatarRef.current.value });
     props.onClose();
-    updateAvatar();
   }
 
   function handleClose() {
     // TODO reset inputs
     props.onClose();
   }
-
-  React.useEffect(() => {
-    updateAvatar();
-  }, [currentUser]);
 
   return (
     <PopupWithForm
@@ -46,7 +32,7 @@ export default function EditAvatarPopup(props) {
             name='avatar'
             type='url'
             placeholder='Ссылка на картинку'
-            onChange={handleChange}
+            ref={avatarRef}
             required
           />
           <span className='form__input-error-hint avatar-input-error'></span>
