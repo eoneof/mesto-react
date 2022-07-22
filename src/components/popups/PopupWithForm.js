93a20e007951d7b1d2b1ab10468fc19bf8b79a70
Nikolay Ivanov <eoneof@yandex.ru>
2022-07-22 12:55:54 +0300

    fix: enable browser validation
    until custom validation is implemented

--- src/components/popups/PopupWithForm.js
+++ src/components/popups/PopupWithForm.js
@@ -13,7 +13,6 @@ export default function PopupWithForm(props) {
           Закрыть
         </button>
         <form
-          noValidate
           className='form'
           id={props.popupType}
           name={props.popupType}
